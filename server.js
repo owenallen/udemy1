9ffb7f6676109f86ab7b37e189131358e463c5f8
Owen Allen <owenallen@outlook.com>
2016-05-30 19:50:26 -0700

    add

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello Code and JScrDev Test App</h1>');
+    
+}).listen(process.env.PORT);
